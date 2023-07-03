545f39e8524e6f82478148227ba1ec7d9ec01112
Ildar Minigulov <mig.ildar@gmail.com>
2023-07-03 17:41:36 +0300

    проверка свич

--- script.js
+++ script.js
@@ -22,5 +22,22 @@ console.log(person['name']);
 //let answer = confirm('Ты готов?');
 //console.log(answer);
 
-let answer = prompt('Есть ли Вам 18?', 'Да');
-console.log(typeof(null));
+//let answer = prompt('Есть ли Вам 18?', 'Да');
+//console.log(typeof(null));
+
+let numw = 50;
+
+switch (numw) {
+    case numw < 49:
+        console.log('НЕ ВЕРНО!');
+        break;
+    case numw > 100:
+        console.log('МНОГО!');
+        break;
+    case numw > 80:
+        console.log('ВСЕ ЕЩЕ МНОГО!');
+        break;
+    case 50:
+        console.log('ВЕРНО!');
+        break;
+}

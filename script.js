8308de635599f23a46727a79988fcb12a80b66f4
Ildar Minigulov <mig.ildar@gmail.com>
2023-07-03 15:10:14 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//a = 5;
+//console.log(a);
+
+console.log( 4 / 0);
+console.log('Ildar' * 9);
+
+let somthing;
+console.log(somthing);
+
+let person = {
+    name: 'Ildar',
+    age: 39,
+    isMarried: true
+}
+
+console.log(person['name']);
+
+//alert('Hello World!');
+
+//let answer = confirm('Ты готов?');
+//console.log(answer);
+
+//let answer = prompt('Есть ли Вам 18?', 'Да');
+console.log(typeof(null));
